5d6ffc4bb390fee9be52edf4b35482859ed0c104
Nikolas Railan <you@example.com>
2023-08-31 14:44:55 -0300

    adicionando produto ao carrinho(tem bug)

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -15,3 +15,21 @@ export function inicializarCarrinho(){
   botaoFecharCarrinho.addEventListener('click', fecharCarrinho);
   botaoAbrirCarrinho.addEventListener('click', abrirCarrinho);
 }
+
+export function adicionarAoCarrinho(){
+  const containerProdutosCarrinho = document.getElementById('produtos-carrinho');
+
+  const cartaoProdutoCarrinho = `<article class="flex bg-slate-100 rounded-lg p-1 relative">
+  <button id="fechar-carrinho" class="absolute top-0 right-1">
+    <i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"></i>
+  </button>
+  <img src="./assets/img/product-1.jpg" class="h-24 rounded-lg" alt="Carrinho: Camisa larga com bolsos">
+  <div class="py-2">
+    <p class="text-slate-900 text-sm">Camisa Larga com bolsos</p>
+    <p class="text-slate-500 text-xs">Tamanho: M</p>
+    <p class="text-green-700 text-lg">$70</p>
+  </div>
+</article>`;
+
+  containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
+}
